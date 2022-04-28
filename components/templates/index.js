aeeeec52c35cf85c1ceea325e32d45008ae3285b
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-28 15:49:14 +0545

    feat(export): PopularProducts component exported

--- components/templates/index.js
+++ components/templates/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
+export { default as PopularProducts } from "./PopularProducts/PopularProducts";
 export { default as Footer } from "./Footer/Footer";
