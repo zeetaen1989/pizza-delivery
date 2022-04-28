5c9b2a65b1da1e16d6f742e14471836bbec9f03a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-28 15:00:04 +0545

    feat(data): slider data created

---
+++ data/slider-data.js
@@ -0,0 +1,20 @@
+export const imgSlider = [
+  {
+    id: 1,
+    img: "/images/slide1.png",
+    title: "Hot & Spicy",
+    percentage: "50%",
+  },
+  {
+    id: 2,
+    img: "/images/slide2.png",
+    title: "Sweet & Sour",
+    percentage: "30%",
+  },
+  {
+    id: 3,
+    img: "/images/slide3.png",
+    title: "Spicy & Tasty",
+    percentage: "25%",
+  },
+];
