216c32787fe8f6a734bb72139711b9617fc174bf
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-28 10:58:55 +0545

    feat(components): Image component removed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@styles/Home.module.scss";
 
 const Home = () => {
@@ -14,7 +13,7 @@ const Home = () => {
         <meta name="keywords" content="FoodLand, Food, Food Delivery" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>HomePage</main>
+      <main></main>
     </div>
   );
 };
