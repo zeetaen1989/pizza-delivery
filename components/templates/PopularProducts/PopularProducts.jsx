8249dfb067bb740199b7d2f35591d6eeb8f699ef
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-28 15:50:02 +0545

    feat(component): PopularProducts component created

---
+++ components/templates/PopularProducts/PopularProducts.jsx
@@ -0,0 +1,10 @@
+import styles from "./PopularProducts.module.scss";
+
+const PopularProducts = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Our Popular Pizzas</h1>
+    </section>
+  );
+};
+export default PopularProducts;
