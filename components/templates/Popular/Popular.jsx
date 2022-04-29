166126b2a3ee42af5b6e51b3940d1930cdaa260d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 11:49:06 +0545

    refactor

--- components/templates/Popular/Popular.jsx
+++ components/templates/Popular/Popular.jsx
@@ -1,4 +1,5 @@
-import { Header, PopularProducts } from "@components/elements";
+import { Header, ProductCard } from "@components/elements";
+import { popularProducts } from "@data/products-data";
 import styles from "./Popular.module.scss";
 
 const Popular = () => {
@@ -9,7 +10,18 @@ const Popular = () => {
         subtitle="What Our Customers Love"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
       />
-      <PopularProducts />
+      <div className={styles.products}>
+        {popularProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.img}
+            title={product.title}
+            price={product.price}
+            rating={product.rating}
+            description={product.description}
+          />
+        ))}
+      </div>
     </section>
   );
 };
