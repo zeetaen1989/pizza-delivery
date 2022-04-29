3e1a974ea9d6f3bb28d0f8aa44142d6fcc781ecc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 20:54:06 +0545

    feat(markups): markup created

--- components/elements/ProductCard/ProductCard.jsx
+++ components/elements/ProductCard/ProductCard.jsx
@@ -1,19 +1,35 @@
+import Link from "next/link";
 import Image from "next/image";
+import { FaHeart, FaSearch } from "react-icons/fa";
+
 import { Ratings, AddToCartBtn } from "@components/elements";
 import styles from "./ProductCard.module.scss";
 
-const Products = ({ image, title, price, rating, description }) => {
+const Products = ({ image, title, description, price, rating, reviews }) => {
   return (
     <div className={styles.products}>
-      <figure>
-        <Image src={image} alt={title} height="250" width="250" />
-      </figure>
-      <figcaption>
+      <section className={styles.products__card}>
+        <figure className={styles.card__img}>
+          <Image src={image} alt={title} height="250" width="250" />
+        </figure>
+        <figcaption className={styles.card__overlay}>
+          <div className={styles.icons}>
+            <FaHeart style={{ cursor: "pointer" }} />
+            <Link href="/product/:id" passHref>
+              <FaSearch style={{ cursor: "pointer" }} />
+            </Link>
+          </div>
+        </figcaption>
+      </section>
+      <article>
         <h3>{title}</h3>
         <span className={styles.price}>${price}</span>
-        <Ratings rating={rating} />
+        <div className={styles.review}>
+          <Ratings rating={rating} />
+          <a href="#">({reviews} Reviews)</a>
+        </div>
         <p>{description}</p>
-      </figcaption>
+      </article>
       <AddToCartBtn />
     </div>
   );
