c7825913281aa4558307c79b1ed2dc461ec02611
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 11:48:22 +0545

    feat(export): ProductCard component exported

--- components/elements/index.js
+++ components/elements/index.js
@@ -1,5 +1,4 @@
 export { default as Announcement } from "./Announcement/Announcement";
 export { default as Header } from "./Header/Header";
 export { default as Ratings } from "./Ratings/Ratings";
-export { default as PopularProducts } from "./PopularProducts/PopularProducts";
-export { default as Products } from "./Products/Products";
+export { default as ProductCard } from "./ProductCard/ProductCard";
