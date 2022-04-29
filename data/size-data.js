9468f373bc75bbe765e8ccf5e09ceee9f003626a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 15:49:03 +0545

    feat(data): new data info created

---
+++ data/size-data.js
@@ -0,0 +1,23 @@
+export const sizeItems = [
+  {
+    id: 1,
+    title: "Small",
+    image: "/images/size.png",
+    height: "30",
+    width: "30",
+  },
+  {
+    id: 2,
+    title: "Medium",
+    image: "/images/size.png",
+    height: "45",
+    width: "45",
+  },
+  {
+    id: 3,
+    title: "Large",
+    image: "/images/size.png",
+    height: "60",
+    width: "60",
+  },
+];
