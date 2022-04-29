a479f54c2c8abf63a2f3236bfcd8bbb036feee3d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 22:35:59 +0545

    feat(page): cart page markups added

--- pages/cart.js
+++ pages/cart.js
@@ -1,4 +1,22 @@
-const cart = () => {
-  return <div>cart</div>;
+import Head from "next/head";
+import { Cart } from "@components/layouts";
+
+const CartPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Cart || </title>
+        <meta
+          name="description"
+          content="This is a shopping cart page display all the items in the cart"
+        />
+        <meta
+          name="keywords"
+          content="Popular Products, Products, Pizza, Food, Food Delivery"
+        />
+      </Head>
+      <Cart />
+    </>
+  );
 };
-export default cart;
+export default CartPage;
