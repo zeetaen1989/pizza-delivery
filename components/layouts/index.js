ed851942b37cd06241edc0b52aa94a8a3be58790
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 11:48:41 +0545

    feat(export): Products component exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -1,2 +1,2 @@
 export { default as Layout } from "./Layout/Layout";
-export { default as ProductsPage } from "./ProductsPage/ProductsPage";
+export { default as Products } from "./Products/Products";
