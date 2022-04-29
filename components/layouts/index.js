8382825eeb0f77e2a746488005dd99f3c55f59fe
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 22:35:36 +0545

    feat(export): Product and Cart components exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -1,2 +1,4 @@
 export { default as Layout } from "./Layout/Layout";
 export { default as Products } from "./Products/Products";
+export { default as Product } from "./Product/Product";
+export { default as Cart } from "./Cart/Cart";
