c07a76b4257300505dd1914f533f9de14eda3583
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-29 15:46:34 +0545

    feat(export): AddToCartBtn component exported

--- components/elements/index.js
+++ components/elements/index.js
@@ -2,3 +2,4 @@ export { default as Announcement } from "./Announcement/Announcement";
 export { default as Header } from "./Header/Header";
 export { default as Ratings } from "./Ratings/Ratings";
 export { default as ProductCard } from "./ProductCard/ProductCard";
+export { default as AddToCartBtn } from "./AddToCartBtn/AddToCartBtn";
