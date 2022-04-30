ed29705b8f2a41fd233113bb73db07cf51e3d8e2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-30 16:28:55 +0545

    feat(cart page markup): added

--- components/layouts/Cart/Cart.jsx
+++ components/layouts/Cart/Cart.jsx
@@ -1,61 +1,116 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { ImBin } from "react-icons/im";
+
+import { Counter } from "@components/elements";
 import styles from "./Cart.module.scss";
 
 const Cart = () => {
+  const [remove, setRemove] = useState(false);
+
+  const handleRemove = () => {
+    setRemove(true);
+  };
+
   return (
     <section className={styles.container}>
-      <div className={styles.left}>
-        <article className={styles.left__col}>
-          <h1>Product</h1>
-          <Image
-            src="/images/Greek-Pizza.png"
-            alt="Greek Pizza"
-            height="100"
-            width="100"
-          />
+      <h1>My Shopping Cart</h1>
+      <div className={styles.bottom}>
+        <article className={styles.left}>
+          <table>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Name</th>
+                <th>Extras</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Image
+                    src="/images/Greek-Pizza.png"
+                    alt="Greek Pizza"
+                    height="100"
+                    width="100"
+                  />
+                </td>
+                <td>Greek Pizza</td>
+                <td>Mozerella, Mushroom, Spicey Sauce</td>
+                <td>$ 19.95</td>
+                <td>
+                  <div className={styles.counter}>
+                    <Counter />
+                  </div>
+                </td>
+                <td>$ 49.95</td>
+                <td>
+                  <ImBin className={styles.bin} />
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>
+                  <Image
+                    src="/images/Greek-Pizza.png"
+                    alt="Greek Pizza"
+                    height="100"
+                    width="100"
+                  />
+                </td>
+                <td>Greek Pizza</td>
+                <td>Mozerella, Mushroom, Spicey Sauce</td>
+                <td>$ 19.95</td>
+                <td>
+                  <div className={styles.counter}>
+                    <Counter />
+                  </div>
+                </td>
+                <td>$ 49.95</td>
+                <td>
+                  <ImBin className={styles.bin} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </article>
-        <article className={styles.left_col}>
-          <h1>Name</h1>
-          <p>Greek Pizza</p>
-        </article>
-        <article className={styles.left_col}>
-          <h1>Extras</h1>
-          <p>Mozerella, Mushroom, Tomato Sauce</p>
-        </article>
-        <article className={styles.left_col}>
-          <h1>Price</h1>
-          <span>$ 19.95</span>
-        </article>
-        <article className={styles.left_col}>
-          <h1>Quantity</h1>
-          <span>3</span>
-        </article>
-        <article className={styles.left_col}>
-          <h1>Total</h1>
-          <span>$ 49.95</span>
-        </article>
+        <aside className={styles.right}>
+          <h1>Cart Summary</h1>
+          <hr />
+          <div className={styles.right__summary}>
+            <p>Subtotal:</p>
+            <span>$ 49.95</span>
+          </div>
+          <div className={styles.right__summary}>
+            <p>Delivery:</p>
+            <span>$ 5.50</span>
+          </div>
+          <div className={styles.right__summary}>
+            <p>Discount:</p>
+            <span>$ 0.00</span>
+          </div>
+          <hr />
+          <div className={styles.right__summary}>
+            <p>Total:</p>
+            <span>$ 55.45</span>
+          </div>
+          <Link href="/orders/:id" passHref>
+            <button className={styles.btn}>
+              Checkout Now
+              <div className={styles.icon}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                  <path d="M168 336C181.3 336 192 346.7 192 360C192 373.3 181.3 384 168 384H120C106.7 384 96 373.3 96 360C96 346.7 106.7 336 120 336H168zM360 336C373.3 336 384 346.7 384 360C384 373.3 373.3 384 360 384H248C234.7 384 224 373.3 224 360C224 346.7 234.7 336 248 336H360zM512 32C547.3 32 576 60.65 576 96V416C576 451.3 547.3 480 512 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H512zM512 80H64C55.16 80 48 87.16 48 96V128H528V96C528 87.16 520.8 80 512 80zM528 224H48V416C48 424.8 55.16 432 64 432H512C520.8 432 528 424.8 528 416V224z" />
+                </svg>
+              </div>
+            </button>
+          </Link>
+        </aside>
       </div>
-      <aside className={styles.right}>
-        <h1>Cart Summary</h1>
-        <hr />
-        <div className={styles.right__summary}>
-          <p>Subtotal:</p>
-          <span>$ 49.95</span>
-        </div>
-        <div className={styles.right__summary}>
-          <p>Delivery:</p>
-          <span>$ 5.50</span>
-        </div>
-        <div className={styles.right__summary}>
-          <p>Discount:</p>
-          <span>$ 0.00</span>
-        </div>
-        <hr />
-        <div className={styles.right__summary}>
-          <p>Total:</p>
-          <span>$ 55.45</span>
-        </div>
-      </aside>
     </section>
   );
 };
