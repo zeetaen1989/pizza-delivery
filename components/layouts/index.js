34b56fec001ff23ea766e85ca83ec77bdf3764eb
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-30 16:30:42 +0545

    feat(export): Orders component exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -2,3 +2,4 @@ export { default as Layout } from "./Layout/Layout";
 export { default as Products } from "./Products/Products";
 export { default as Product } from "./Product/Product";
 export { default as Cart } from "./Cart/Cart";
+export { default as Orders } from "./Orders/Orders";
