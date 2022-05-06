f2edb63eb888c8f40a7c69a9ae5a24bbfd455023
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-06 13:11:17 +0545

    feat(export): About, Locations, Testimonials components exported

--- components/templates/index.js
+++ components/templates/index.js
@@ -1,4 +1,7 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
+export { default as About } from "./About/About";
 export { default as Popular } from "./Popular/Popular";
+export { default as Locations } from "./Locations/Locations";
+export { default as Testimonials } from "./Testimonials/Testimonials";
 export { default as Footer } from "./Footer/Footer";
