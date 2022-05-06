c06dc761488a67e297f7e786137ece92e94ea16c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-06 22:33:08 +0545

    feat(export): Contact component exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -3,3 +3,4 @@ export { default as Products } from "./Products/Products";
 export { default as Product } from "./Product/Product";
 export { default as Cart } from "./Cart/Cart";
 export { default as Orders } from "./Orders/Orders";
+export { default as Contact } from "./Contact/Contact";
