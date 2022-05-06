c06f30decff5eccbae5b34e30a9b3c3fd02107b3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-06 16:28:16 +0545

    feat(export): Newsletter component exported

--- components/templates/index.js
+++ components/templates/index.js
@@ -4,4 +4,5 @@ export { default as About } from "./About/About";
 export { default as Popular } from "./Popular/Popular";
 export { default as Locations } from "./Locations/Locations";
 export { default as Testimonials } from "./Testimonials/Testimonials";
+export { default as Newsletter } from "./Newsletter/Newsletter";
 export { default as Footer } from "./Footer/Footer";
