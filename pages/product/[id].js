301dbd11cf5ef9012d142cc49da6cacba365d97c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-07 20:33:31 +0545

    feat: id slug single product page created

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import { Product } from "@components/layouts";
 
-const ProductPage = () => {
-  return <Product />;
+const ProductPage = ({ pizza }) => {
+  return <Product pizza={pizza} />;
 };
 export default ProductPage;
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(
+    `http://localhost:3000/api/products/${params.id}`
+  );
+  return {
+    props: {
+      pizza: res.data,
+    },
+  };
+};
