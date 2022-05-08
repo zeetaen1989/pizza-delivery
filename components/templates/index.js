d7f1195d44d1321510c5d2a0f990e38e2b5d0573
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-09 00:21:43 +0545

    feat: ProductList exported

--- components/templates/index.js
+++ components/templates/index.js
@@ -2,6 +2,7 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
 export { default as About } from "./About/About";
 export { default as Popular } from "./Popular/Popular";
+export { default as ProductList } from "./ProductList/ProductList";
 export { default as Locations } from "./Locations/Locations";
 export { default as Testimonials } from "./Testimonials/Testimonials";
 export { default as Newsletter } from "./Newsletter/Newsletter";
