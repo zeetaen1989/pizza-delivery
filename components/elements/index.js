cdda6d95a54dd10303011f73b26bfa452ceb054d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-08 22:15:03 +0545

    feat: Counter component exported

--- components/elements/index.js
+++ components/elements/index.js
@@ -1,6 +1,6 @@
 export { default as AddToCartBtn } from "./AddToCartBtn/AddToCartBtn";
 export { default as Announcement } from "./Announcement/Announcement";
-export { default as Counter } from "./Counter/Counter";
 export { default as Header } from "./Header/Header";
+export { default as Counter } from "./Counter/Counter";
 export { default as Ratings } from "./Ratings/Ratings";
 export { default as ProductCard } from "./ProductCard/ProductCard";
