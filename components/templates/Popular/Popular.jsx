e040137fcf6a75bb8b4263321e9fb5ce1695161a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-09 21:06:18 +0545

    feat: markup for see all products button added

--- components/templates/Popular/Popular.jsx
+++ components/templates/Popular/Popular.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Header } from "@components/elements";
 import { ProductList } from "..";
 import styles from "./Popular.module.scss";
@@ -11,6 +12,15 @@ const Popular = ({ pizzaList }) => {
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
       />
       <ProductList pizzaList={pizzaList} />
+      <Link href="/products" passHref>
+        <a className={styles.btn}>
+          <span className={styles.text}>See All Products</span>
+          <svg viewBox="0 0 13 10" height="10px" width="15px">
+            <path d="M1,5 L11,5"></path>
+            <polyline points="8 1 12 5 8 9"></polyline>
+          </svg>
+        </a>
+      </Link>
     </section>
   );
 };
