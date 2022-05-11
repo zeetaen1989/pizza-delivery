439287db3b1448cd6530f64768674065039b161e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-11 22:19:33 +0545

    feat: OrderBtn component created

---
+++ components/elements/OrderBtn/OrderBtn.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "./OrderBtn.module.scss";
+
+const OrderBtn = ({ text }) => {
+  return (
+    <Link href="/products" passHref>
+      <a className={styles.btn__order}>
+        <span className={styles.text}>{text}</span>
+        <svg viewBox="0 0 13 10" height="10px" width="15px">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="8 1 12 5 8 9"></polyline>
+        </svg>
+      </a>
+    </Link>
+  );
+};
+export default OrderBtn;
