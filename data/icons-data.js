ec36d39a1e6d8544f79c4f532080c5a748ba25b5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-13 17:49:10 +0545

    feta: data for icons added

---
+++ data/icons-data.js
@@ -0,0 +1,30 @@
+export const iconsData = [
+  {
+    id: 1,
+    title: "Shopping Bag",
+    img: "/images/cart.svg",
+    height: "50",
+    width: "50",
+  },
+  {
+    id: 2,
+    title: "Contact Info",
+    img: "/images/contact.svg",
+    height: "40",
+    width: "40",
+  },
+  {
+    id: 3,
+    title: "Pay",
+    img: "/images/pay.svg",
+    height: "40",
+    width: "40",
+  },
+  {
+    id: 4,
+    title: "Deliver",
+    img: "/images/deliver.svg",
+    height: "60",
+    width: "60",
+  },
+];
