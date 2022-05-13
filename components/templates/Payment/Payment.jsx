1fa4a7611d4b95c4c82262fc5410da0339d14769
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-13 17:48:54 +0545

    feat: components created

---
+++ components/templates/Payment/Payment.jsx
@@ -0,0 +1,10 @@
+import styles from "./Payment.module.scss";
+
+const Payment = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Payment Details</h1>
+    </div>
+  );
+};
+export default Payment;
