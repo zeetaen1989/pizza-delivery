d4ef1a91179b325c7d7ebfcc44510676cd963c20
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-13 17:47:30 +0545

    feat: Orders export removed

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -1,5 +1,4 @@
 export { default as Layout } from "./Layout/Layout";
 export { default as Product } from "./Product/Product";
 export { default as Cart } from "./Cart/Cart";
-export { default as Orders } from "./Orders/Orders";
 export { default as Contact } from "./Contact/Contact";
