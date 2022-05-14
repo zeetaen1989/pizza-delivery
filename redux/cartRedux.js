5dbef2d98386c4710cefd80b66bc9cb016f95c95
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-14 22:17:23 +0545

    feat: action creators added to the reducers

--- redux/cartRedux.js
+++ redux/cartRedux.js
@@ -8,11 +8,40 @@ const cartSlice = createSlice({
     total: 0,
   },
   reducers: {
-    addProduct: (state, action) => {
-      state.products.push(action.payload);
+    addProduct: (state, { payload }) => {
+      state.products.push(payload);
       state.quantity += 1;
-      state.total += action.payload.price * action.payload.count;
+      state.total += payload.price * payload.count;
     },
+    removeProduct: (state, { payload }) => {
+      // state.products.filter((product) => product._id !== payload);
+      state.products.splice(payload, 1);
+      state.quantity -= 1;
+      state.total -= payload.price * payload.count;
+    },
+    increaseProduct: (state, { payload }) => {
+      // state.products[payload].count += 1;
+      // state.quantity += 1;
+      // state.total += state.products[payload].price;
+
+      const cartItem = state.products.find((item) => item.id === payload._id);
+      cartItem.quantity = cartItem.quantity + 1;
+    },
+    decreaseProduct: (state, { payload }) => {
+      state.products[payload].count -= 1;
+      state.quantity -= 1;
+      state.total -= state.products[payload].price;
+    },
+    // calculateTotals: (state) => {
+    //   let count = 0;
+    //   let total = 0;
+    //   state.products.forEach((product) => {
+    //     count += product.count;
+    //     total += product.count * product.price;
+    //   });
+    //   state.quantity = count;
+    //   state.total = total;
+    // },
     resetCart: (state) => {
       state.products = [];
       state.quantity = 0;
@@ -21,5 +50,12 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addProduct, resetCart } = cartSlice.actions;
+export const {
+  addProduct,
+  removeProduct,
+  increaseProduct,
+  decreaseProduct,
+  calculateTotals,
+  resetCart,
+} = cartSlice.actions;
 export default cartSlice.reducer;
