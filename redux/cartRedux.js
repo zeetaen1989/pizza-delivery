29f77d8162e7ceac91b4f05514d46624493b5aad
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-15 15:52:46 +0545

    feat: add calculateTotals reducer to update the total variable on each render of products variable

--- redux/cartRedux.js
+++ redux/cartRedux.js
@@ -11,18 +11,16 @@ const cartSlice = createSlice({
     addProduct: (state, { payload }) => {
       state.products.push(payload);
       state.quantity += 1;
-      state.total += payload.price * payload.count;
     },
     removeProduct: (state, { payload }) => {
-      // state.products.filter((product) => product._id !== payload);
-      state.products.splice(payload, 1);
+      state.products = state.products.filter(
+        (product) => product._id !== payload
+      );
       state.quantity -= 1;
-      state.total -= payload.price * payload.count;
     },
     increaseProduct: (state, { payload }) => {
       // state.products[payload].count += 1;
       // state.quantity += 1;
-      // state.total += state.products[payload].price;
 
       const cartItem = state.products.find((item) => item.id === payload._id);
       cartItem.quantity = cartItem.quantity + 1;
@@ -30,18 +28,17 @@ const cartSlice = createSlice({
     decreaseProduct: (state, { payload }) => {
       state.products[payload].count -= 1;
       state.quantity -= 1;
-      state.total -= state.products[payload].price;
     },
-    // calculateTotals: (state) => {
-    //   let count = 0;
-    //   let total = 0;
-    //   state.products.forEach((product) => {
-    //     count += product.count;
-    //     total += product.count * product.price;
-    //   });
-    //   state.quantity = count;
-    //   state.total = total;
-    // },
+    calculateTotals: (state) => {
+      let count = 0;
+      let total = 0;
+      state.products.forEach((product) => {
+        count += product.count;
+        total += product.count * product.price;
+      });
+      state.quantity = count;
+      state.total = total;
+    },
     resetCart: (state) => {
       state.products = [];
       state.quantity = 0;
