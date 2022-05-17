bd7924ccdc7dd1e43fc5d860a7b70534a16838fa
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-17 20:29:21 +0545

    feat: delivery section markup added

---
+++ components/templates/Delivery/Delivery.jsx
@@ -0,0 +1,11 @@
+import styles from "./Delivery.module.scss";
+
+const Delivery = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Thank You For Choosing Us!!!</h1>
+      <h2>Your Pizza Is On The Way...</h2>
+    </div>
+  );
+};
+export default Delivery;
