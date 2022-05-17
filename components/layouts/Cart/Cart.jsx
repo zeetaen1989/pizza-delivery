dfe8c6c112a62be3c0fa7cf4a22528d609eb2ed7
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-17 20:25:13 +0545

    feat: TabItem and TabContent components imported and added to the markup

--- components/layouts/Cart/Cart.jsx
+++ components/layouts/Cart/Cart.jsx
@@ -1,45 +1,95 @@
-import Image from "next/image";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
-import { CartItems, CustomerInfo } from "@components/templates";
-import { iconsData } from "@data/icons-data";
+import {
+  CartItems,
+  CustomerInfo,
+  Delivery,
+  Payment,
+} from "@components/templates";
+import { TabContent, TabItem } from "@components/elements";
 import styles from "./Cart.module.scss";
 
 const Cart = () => {
   const { products, total } = useSelector((state) => state.cart);
 
-  const status = 0;
-
-  const iconStatus = (index) => {
-    if (index - status < 1) return styles.completed;
-    if (index - status === 1) return styles.pending;
-    if (index - status > 1) return styles.incomplete;
-  };
+  const [activeTab, setActiveTab] = useState("1");
 
   return (
     <section className={styles.container}>
-      <div className={styles.top}>
-        {iconsData.map((icon) => (
-          <figure key={icon.id} className={iconStatus(icon.id)}>
-            <Image
-              src={icon.img}
-              alt={icon.title}
-              height={icon.height}
-              width={icon.width}
+      <div className={styles.tabs}>
+        <ul className={styles.nav}>
+          <TabItem
+            id="1"
+            img="/images/cart.svg"
+            title="Shopping Cart"
+            height="50"
+            width="50"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <hr />
+          <TabItem
+            id="2"
+            img="/images/contact.svg"
+            title="Contact Info"
+            height="40"
+            width="40"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <hr />
+          <TabItem
+            id="3"
+            img="/images/pay.svg"
+            title="Contact Info"
+            height="40"
+            width="40"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+          <hr />
+          <TabItem
+            id="4"
+            img="/images/deliver.svg"
+            title="Delivery"
+            height="60"
+            width="60"
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </ul>
+        <div className={styles.tab__content}>
+          <TabContent id="1" activeTab={activeTab}>
+            <CartItems
+              products={products}
+              total={total}
+              setActiveTab={setActiveTab}
             />
-            <figure className={styles.checked__icon}>
-              <Image
-                src="/images/checked.svg"
-                alt="Checked"
-                height="10"
-                width="10"
-              />
-            </figure>
-          </figure>
-        ))}
+          </TabContent>
+          <TabContent id="2" activeTab={activeTab}>
+            <CustomerInfo
+              products={products}
+              total={total}
+              setActiveTab={setActiveTab}
+            />
+          </TabContent>
+          <TabContent id="3" activeTab={activeTab}>
+            <Payment
+              products={products}
+              total={total}
+              setActiveTab={setActiveTab}
+            />
+          </TabContent>
+          <TabContent id="4" activeTab={activeTab}>
+            <Delivery
+              products={products}
+              total={total}
+              setActiveTab={setActiveTab}
+            />
+          </TabContent>
+        </div>
       </div>
-      <CartItems products={products} total={total} />
-      <CustomerInfo products={products} total={total} />
     </section>
   );
 };
