4079769f47a9a6b9dd2e2d4e5bf2415ace296183
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-20 19:48:17 +0545

    feat: export Register and Login components

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -2,3 +2,5 @@ export { default as Layout } from "./Layout/Layout";
 export { default as Product } from "./Product/Product";
 export { default as Cart } from "./Cart/Cart";
 export { default as Contact } from "./Contact/Contact";
+export { default as Register } from "./Register/Register";
+export { default as Login } from "./Login/Login";
