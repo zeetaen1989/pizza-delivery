9a7f5edf065f2af6549e33b4a521e608d18075d1
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-20 19:47:59 +0545

    feat: logic to render and display repetetive layout only when the route path is not either register or login added

--- components/layouts/Layout/Layout.jsx
+++ components/layouts/Layout/Layout.jsx
@@ -1,13 +1,24 @@
 import { Announcement } from "@components/elements";
 import { Footer, Navbar } from "@components/templates";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  const path = router.pathname.split("/")[1];
+
   return (
     <>
-      <Announcement />
-      <Navbar />
-      {children}
-      <Footer />
+      {path === "register" || path === "login" ? (
+        children
+      ) : (
+        <>
+          <Announcement />
+          <Navbar />
+          {children}
+          <Footer />
+        </>
+      )}
     </>
   );
 };
