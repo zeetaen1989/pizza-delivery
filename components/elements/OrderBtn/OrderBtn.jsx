de93bd02c49efa5e177674b38f75b3f36db309eb
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-20 19:46:19 +0545

    feat: refactor

--- components/elements/OrderBtn/OrderBtn.jsx
+++ components/elements/OrderBtn/OrderBtn.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import styles from "./OrderBtn.module.scss";
 
-const OrderBtn = ({ text, link }) => {
+const OrderBtn = ({ text }) => {
   return (
-    <Link href={`/${link}`} passHref>
+    <Link href="/products" passHref>
       <a className={styles.btn__order}>
         <span className={styles.text}>{text}</span>
         <svg viewBox="0 0 13 10" height="10px" width="15px">
