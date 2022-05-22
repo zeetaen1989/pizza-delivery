0cf6261442caaa96ef4bbace8c57839402a57f03
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-22 17:18:32 +0545

    feat: ProductCard component imported and added to the markup as well as logic added to display only six items

--- components/templates/Popular/Popular.jsx
+++ components/templates/Popular/Popular.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import { Header, OrderBtn } from "@components/elements";
-import { ProductList } from "..";
+import { Header, OrderBtn, ProductCard } from "@components/elements";
 import styles from "./Popular.module.scss";
 
 const Popular = ({ pizzaList }) => {
@@ -11,7 +10,11 @@ const Popular = ({ pizzaList }) => {
         subtitle="What Our Customers Love"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
       />
-      <ProductList pizzaList={pizzaList} />
+      <div className={styles.container__products}>
+        {pizzaList.slice(0, 6).map((pizza) => (
+          <ProductCard key={pizza.id} pizza={pizza} {...pizza} />
+        ))}
+      </div>
       <OrderBtn text="See All Menu" />
     </section>
   );
