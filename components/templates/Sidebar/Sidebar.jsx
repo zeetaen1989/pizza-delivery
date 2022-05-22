d1da203b753d77f39a76383c0edb064a0da8206e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-22 17:22:45 +0545

    feat: markups for sidebar component added

--- components/templates/Sidebar/Sidebar.jsx
+++ components/templates/Sidebar/Sidebar.jsx
@@ -1,6 +1,35 @@
 import styles from "./Sidebar.module.scss";
 
 const Sidebar = () => {
-  return <div className={styles.sidebar}>Sidebar</div>;
+  const catList = [
+    "All",
+    "Italian",
+    "American",
+    "Canadian",
+    "Greek",
+    "Vegan",
+    "Miscellaneous",
+  ];
+
+  return (
+    <div className={styles.sidebar}>
+      <section className={styles.search}>
+        <input type="text" placeholder="Search" />
+      </section>
+      <section className={styles.category}>
+        <h3>Categories</h3>
+        <ul>
+          {catList.map((list) => (
+            <li key={list}>{list}</li>
+          ))}
+        </ul>
+      </section>
+      <section className={styles.price}>
+        <h3>Price</h3>
+        <label htmlFor="price">$5 - $50</label>
+        <input type="range" name="price" id="price" />
+      </section>
+    </div>
+  );
 };
 export default Sidebar;
