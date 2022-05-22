339d676d97ceb06e3c5416f85156ced4b0f42df8
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-22 23:32:36 +0545

    feat: classname modified

--- components/elements/OrderBtn/OrderBtn.jsx
+++ components/elements/OrderBtn/OrderBtn.jsx
@@ -4,7 +4,7 @@ import styles from "./OrderBtn.module.scss";
 const OrderBtn = ({ text }) => {
   return (
     <Link href="/menu" passHref>
-      <a className={styles.btn__order}>
+      <a className={styles.btn}>
         <span className={styles.text}>{text}</span>
         <svg viewBox="0 0 13 10" height="10px" width="15px">
           <path d="M1,5 L11,5"></path>
