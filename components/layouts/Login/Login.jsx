2aa7211c88aeb7c31fad2eefe76350853a1944b2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-23 22:27:54 +0545

    feat: markup for images and login button added

--- components/layouts/Login/Login.jsx
+++ components/layouts/Login/Login.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import { OrderBtn } from "@components/elements";
 import styles from "./Login.module.scss";
+import Image from "next/image";
 
 const Login = () => {
   return (
     <section className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.left}>
+          <Image
+            src="/images/delivery.png"
+            alt="Delivery"
+            height="500"
+            width="700"
+          />
           <p>
-            A place where you can find the best pizza in town from various
-            regions.
+            Get your favorite pizza delivered to your doorstep in just a few
+            minutes.
           </p>
         </div>
         <div className={styles.right}>
@@ -46,7 +53,13 @@ const Login = () => {
                 <input type="checkbox" name="login" id="remember" />
                 <label htmlFor="remember">Remember Me</label>
               </div>
-              <OrderBtn text="Login" />
+              <a className={styles.btn}>
+                <span className={styles.text}>Login</span>
+                <svg viewBox="0 0 13 10" height="10px" width="15px">
+                  <path d="M1,5 L11,5"></path>
+                  <polyline points="8 1 12 5 8 9"></polyline>
+                </svg>
+              </a>
             </form>
             <div className={styles.bottom__content}>
               <p>Don&apos;t have an account?</p>
