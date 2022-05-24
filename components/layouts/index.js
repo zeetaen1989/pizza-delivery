c66db327d2d04c87fda33b0259fd2d9b513d1b49
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-24 22:26:42 +0545

    feat: Orders component exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -4,3 +4,4 @@ export { default as Cart } from "./Cart/Cart";
 export { default as Contact } from "./Contact/Contact";
 export { default as Register } from "./Register/Register";
 export { default as Login } from "./Login/Login";
+export { default as Orders } from "./Orders/Orders";
