3c273357b5a3f23378f9b399616291d729c5ae8b
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-25 16:34:46 +0545

    refactor: checked icon element deleted

--- components/elements/TabItem/TabItem.jsx
+++ components/elements/TabItem/TabItem.jsx
@@ -21,14 +21,6 @@ const TabItem = ({
         className={activeTab === id ? `${styles.active}` : `${styles.inactive}`}
       >
         <Image src={img} alt={title} height={height} width={width} />
-        <figure className={styles.checked__icon}>
-          <Image
-            src="/images/checked.svg"
-            alt="Checked"
-            height="20"
-            width="20"
-          />
-        </figure>
       </figure>
     </li>
   );
