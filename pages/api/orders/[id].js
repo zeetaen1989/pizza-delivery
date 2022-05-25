d7cad09358b4ac6ae417a8fd7da4c0f38199ab63
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-25 16:39:48 +0545

    feat: GET, PUT and DELETE methods added to modify the items added

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -10,8 +10,22 @@ const handler = async (req, res) => {
   await dbConnect();
 
   if (method === "GET") {
+    try {
+      const order = await Order.findById(id);
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
   if (method === "PUT") {
+    try {
+      const order = await Order.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
   if (method === "DELETE") {
   }
