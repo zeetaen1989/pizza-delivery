ffac1f14c7cddc4602efa8a960694937f600733e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-25 16:37:05 +0545

    feat: Error page exported

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -5,3 +5,4 @@ export { default as Contact } from "./Contact/Contact";
 export { default as Register } from "./Register/Register";
 export { default as Login } from "./Login/Login";
 export { default as Orders } from "./Orders/Orders";
+export { default as Error } from "./Error/Error";
