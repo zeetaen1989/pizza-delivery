aa5d3d0d1261f9ee4a45ae53067c596784d72eb0
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-25 21:14:29 +0545

    feat: data modified

--- data/slider-data.js
+++ data/slider-data.js
@@ -1,32 +1,37 @@
 export const imgSlider = [
   {
     id: 1,
-    img: "/images/slide1.png",
-    title: "Hot & Spicy",
-    percentage: "50%",
+    img: "/images/slider-delivery.svg",
+    title: "Regional Taste Pizza Delivery Service",
+    subtitle:
+      "Get a taste of pizza from various regions delivered to your doorstep. Wide range of selection with your choice of toppings and sauces.",
   },
   {
     id: 2,
-    img: "/images/slide2.png",
-    title: "Cheesy & Tasty",
-    percentage: "30%",
+    img: "/images/slider-order.svg",
+    title: "Order From Anywhere",
+    subtitle:
+      "Order pizza from the comfort of your home or office. Select from wide range of pizzas and toppings.",
   },
   {
     id: 3,
-    img: "/images/slide3.png",
-    title: "Sweet & Tasty",
-    percentage: "25%",
+    img: "/images/slider-chef.svg",
+    title: "Fresh Ingredients",
+    subtitle:
+      "Our Chef will prepare your pizza with the freshest ingredients. We guarantee freshness and taste.",
   },
   {
     id: 4,
-    img: "/images/slide4.png",
-    title: "Cheesy & Spicy",
-    percentage: "40%",
+    img: "/images/slider-delivered.svg",
+    title: "Delivered To Your Door",
+    subtitle:
+      "Get your pizza delivered to your doorstep while its hot and tasty. We deliver to your doorstep within 30 minutes. Money back guarantee.",
   },
   {
     id: 5,
-    img: "/images/slide5.png",
-    title: "Sweet & Sour",
-    percentage: "20%",
+    img: "/images/slider-share.svg",
+    title: "Enjoy With Loved Ones",
+    subtitle:
+      "Enjoy hot and tasty pizza with your loved ones. Remember us again if your liked our pizzas. We are always happy to serve you.",
   },
 ];
