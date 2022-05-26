165d7a8b9b44fc0a363338efe159da5ea6c2c4bc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-05-26 21:51:03 +0545

    feat: MongoDB_URL modified

--- util/mongo.js
+++ util/mongo.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
+// check the MongoDB URL
 if (!MONGODB_URL) {
   throw new Error(
     "Please define the MONGODB_URI environment variable inside .env.local"
