ac43ef2507e2052760fed5aca680d7776e90728e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-01 13:35:06 +0545

    feat: export ActiveLink component

--- components/elements/index.js
+++ components/elements/index.js
@@ -8,3 +8,4 @@ export { default as Ratings } from "./Ratings/Ratings";
 export { default as ProductCard } from "./ProductCard/ProductCard";
 export { default as TabItem } from "./TabItem/TabItem";
 export { default as TabContent } from "./TabContent/TabContent";
+export { default as ActiveLink } from "./ActiveLink/ActiveLink";
