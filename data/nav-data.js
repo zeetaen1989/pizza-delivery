0a83edb4a1b3917d2606a9962237bbc073211653
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-01 13:41:47 +0545

    feat: local data for nav links added

---
+++ data/nav-data.js
@@ -0,0 +1,25 @@
+export const linksLeft = [
+  {
+    id: 1,
+    url: "/",
+    text: "Home",
+  },
+  {
+    id: 2,
+    url: "/menu",
+    text: "Menu",
+  },
+];
+
+export const linksRight = [
+  {
+    id: 1,
+    url: "/#locations",
+    text: "Locations",
+  },
+  {
+    id: 2,
+    url: "/contact",
+    text: "Contact Us",
+  },
+];
