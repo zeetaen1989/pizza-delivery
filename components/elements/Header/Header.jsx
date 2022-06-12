cb35dad01c0e095bfb28e9441f61b0a9fc2158a6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-12 09:52:58 +0545

    refactor: markup tags renamed

--- components/elements/Header/Header.jsx
+++ components/elements/Header/Header.jsx
@@ -2,11 +2,11 @@ import styles from "./Header.module.scss";
 
 const Header = ({ title, subtitle, description }) => {
   return (
-    <header className={styles.container}>
+    <section className={styles.container}>
       <span className={styles.top__text}>{title}</span>
       <h1>{subtitle}</h1>
       <p>{description}</p>
-    </header>
+    </section>
   );
 };
 export default Header;
