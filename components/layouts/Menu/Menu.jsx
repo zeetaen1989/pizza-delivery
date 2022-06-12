1f2fac10d2ff31cc32a3147aa499d44ef9851897
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-12 09:53:56 +0545

    feat: props passed to ProductList component is filtered at first to add search functionality beforehand

--- components/layouts/Menu/Menu.jsx
+++ components/layouts/Menu/Menu.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+
 import { Header } from "@components/elements";
 import { ProductList, Sidebar } from "@components/templates";
 import styles from "./Menu.module.scss";
 
 const Menu = ({ pizzaList, allCategories }) => {
   const [menuItems, setMenuItems] = useState(pizzaList);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   const filterItemsByCategory = (category) => {
     if (category === "All") {
@@ -16,28 +22,39 @@ const Menu = ({ pizzaList, allCategories }) => {
   };
 
   const filterItemsByPrice = (price) => {
-    const newItems = pizzaList.filter((pizza) => pizza.prices[0] <= price);
-    setMenuItems(newItems);
+    if (price === 20) {
+      setMenuItems(pizzaList);
+    } else {
+      const newItems = pizzaList.filter((pizza) => pizza.prices[0] <= price);
+      setMenuItems(newItems);
+    }
   };
 
   return (
     <section className={styles.container}>
-      <div className={styles.header}>
+      <header className={styles.header}>
         <Header
           title="Our Menu"
           subtitle="Various Regional Taste"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
         />
-      </div>
-      <div className={styles.content}>
+      </header>
+      <main className={styles.content}>
         <Sidebar
           allCategories={allCategories}
           filterItemsByCategory={filterItemsByCategory}
           filterItemsByPrice={filterItemsByPrice}
+          handleChange={handleChange}
+          searchTerm={searchTerm}
         />
-        <ProductList pizzaList={menuItems} />
-      </div>
+        <ProductList
+          pizzaList={menuItems.filter((menu) =>
+            menu.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          )}
+        />
+      </main>
     </section>
   );
 };
+
 export default Menu;
