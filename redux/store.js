4a58bfee1d147e8ff07d2d6e8936b78c04defc03
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-06-12 09:56:16 +0545

    feat: menuRedux reducer removed from redux store

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
