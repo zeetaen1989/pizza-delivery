a9500eff697ac55d1d3dc0a993934fe272a7ae94
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-12 18:53:30 +0545

    feat: redux slice name modified

---
+++ redux/cartSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addProduct: (state, action) => {
+      const newProduct = action.payload;
+      const existingProduct = state.products.find(
+        (product) => product._id === newProduct._id
+      );
+
+      if (!existingProduct) {
+        state.products.push(newProduct);
+        state.totalQuantity++;
+      } else {
+        existingProduct.quantity++;
+      }
+    },
+    removeProduct: (state, action) => {
+      const id = action.payload;
+      const existingProduct = state.products.find(
+        (product) => product._id === id
+      );
+      state.totalQuantity--;
+
+      if (existingProduct.quantity === 1) {
+        state.products = state.products.filter((product) => product._id !== id);
+      } else {
+        existingProduct.quantity--;
+      }
+    },
+    calculateTotals: (state) => {
+      let total = 0;
+      state.products.forEach((product) => {
+        total += product.quantity * product.price;
+      });
+      state.totalAmount = total;
+    },
+    resetCart: (state) => {
+      state.products = [];
+      state.totalQuantity = 0;
+      state.totalAmount = 0;
+    },
+  },
+});
+
+export const selectAllProducts = (state) => state.cart.products;
+
+export const { addProduct, removeProduct, calculateTotals, resetCart } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
