80929272f55406c62cd18218781a3413651492e3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-14 14:52:22 +0545

    feat: Reviews component's named export added

--- components/templates/index.js
+++ components/templates/index.js
@@ -13,3 +13,4 @@ export { default as Payment } from "./Payment/Payment";
 export { default as Delivery } from "./Delivery/Delivery";
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as HorizontalProductCard } from "./HorizontalProductCard/HorizontalProductCard";
+export { default as Reviews } from "./Reviews/Reviews";
