d57d33e218b040741cbb80abe7f04336b9822f47
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-09-20 14:40:55 +0545

    feat: AuthProvider context imported and added to the markup

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Provider } from "react-redux";
 import { Layout } from "@components/layouts";
+import { AuthProvider } from "context/AuthProvider";
 import { store } from "../redux/store";
 import "@styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </AuthProvider>
   );
 }
 
